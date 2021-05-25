78b8f0b333c0f831bd46822dcd1bb3c175cd1f2b
Piyush Shah <piyush.shah@espressif.com>
2021-05-25 23:57:42 +0530

    utils: Changes to the reset/reboot APIs
    Earlier APIs only allowed reboot to be asynchronous, however, there was a need
    to make even Wi-Fi/factory reset to be asynchronous so that applications can
    get some time to report status or perform other tasks before resetting.
    
    Minor: Removed time service API from header file since it is not part
    of this project

--- src/utils.c
+++ src/utils.c
@@ -22,6 +22,7 @@
 ESP_EVENT_DEFINE_BASE(RMAKER_COMMON_EVENT);
 
 static TimerHandle_t reboot_timer;
+static TimerHandle_t reset_timer;
 
 static void esp_rmaker_reboot_cb(TimerHandle_t handle)
 {
@@ -30,12 +31,17 @@ static void esp_rmaker_reboot_cb(TimerHandle_t handle)
     esp_restart();
 }
 
-esp_err_t esp_rmaker_reboot(uint8_t seconds)
+esp_err_t esp_rmaker_reboot(int8_t seconds)
 {
-    /* If reboot timer already exists, it means that a reboot operation is already in progress.
-     * So, just return an error from here.
-     */
-    if (reboot_timer) {
+    /* If specified time is 0, reboot immediately */
+    if (seconds == 0) {
+        esp_event_post(RMAKER_COMMON_EVENT, RMAKER_EVENT_REBOOT, &seconds, sizeof(seconds), portMAX_DELAY);
+        esp_restart();
+        return ESP_OK;
+    } else if (reboot_timer) {
+        /* If reboot timer already exists, it means that a reboot operation is already in progress.
+         * So, just return an error from here.
+         */
         return ESP_FAIL;
     }
     reboot_timer = xTimerCreate("rmaker_reboot_tm", (seconds * 1000) / portTICK_PERIOD_MS,
@@ -53,17 +59,109 @@ esp_err_t esp_rmaker_reboot(uint8_t seconds)
     return ESP_OK;
 }
 
-esp_err_t esp_rmaker_wifi_reset(uint8_t seconds)
+static esp_err_t __esp_rmaker_wifi_reset(int8_t reboot_seconds)
 {
     esp_wifi_restore();
-    esp_event_post(RMAKER_COMMON_EVENT, RMAKER_EVENT_WIFI_RESET, NULL, 0, portMAX_DELAY);
-    return esp_rmaker_reboot(seconds);
+    if (reboot_seconds >= 0) {
+        esp_rmaker_reboot(reboot_seconds);
+    }
+    return ESP_OK;
 }
 
-esp_err_t esp_rmaker_factory_reset(uint8_t seconds)
+static esp_err_t __esp_rmaker_factory_reset(int8_t reboot_seconds)
 {
     nvs_flash_deinit();
     nvs_flash_erase();
-    esp_event_post(RMAKER_COMMON_EVENT, RMAKER_EVENT_FACTORY_RESET, NULL, 0, portMAX_DELAY);
-    return esp_rmaker_reboot(seconds);
+    if (reboot_seconds >= 0) {
+        esp_rmaker_reboot(reboot_seconds);
+    }
+    return ESP_OK;
+}
+
+static void esp_rmaker_wifi_reset_cb(TimerHandle_t handle)
+{
+    /* (Hack) Using the timer id as reboot seconds */
+    int8_t *reboot_seconds = (int8_t *)pvTimerGetTimerID(handle);
+    if (reboot_seconds) {
+        __esp_rmaker_wifi_reset((int8_t)*reboot_seconds);
+        free(reboot_seconds);
+    } else {
+        __esp_rmaker_wifi_reset(0);
+    }
+    xTimerDelete(reset_timer, 10);
+    reset_timer = NULL;
+}
+
+
+static void esp_rmaker_factory_reset_cb(TimerHandle_t handle)
+{
+    /* (Hack) Using the timer id as reboot seconds */
+    int8_t *reboot_seconds = (int8_t *)pvTimerGetTimerID(handle);
+    if (reboot_seconds) {
+        __esp_rmaker_factory_reset((int8_t)*reboot_seconds);
+        free(reboot_seconds);
+    } else {
+        __esp_rmaker_factory_reset(0);
+    }
+    xTimerDelete(reset_timer, 10);
+    reset_timer = NULL;
+}
+
+static esp_err_t esp_rmaker_start_reset_timer(int8_t reset_seconds, int8_t reboot_seconds, TimerCallbackFunction_t cb)
+{
+    /* If reset timer already exists, it means that a reset operation is already in progress.
+     * So, just return an error from here.
+     */
+    if (reset_timer) {
+        return ESP_FAIL;
+    }
+    /* (Hack) Passing the reboot delay as timer id */
+    int8_t *reboot_delay = calloc(1, sizeof(int8_t));
+    if (reboot_delay) {
+        *reboot_delay = reboot_seconds;
+    }
+    reset_timer = xTimerCreate("rmaker_reset_tm", (reset_seconds * 1000) / portTICK_PERIOD_MS,
+            pdFALSE, (void *)reboot_delay, cb);
+    if (reset_timer) {
+        if (xTimerStart(reset_timer, 10) != pdTRUE) {
+            xTimerDelete(reset_timer, 10);
+            reset_timer = NULL;
+            return ESP_FAIL;
+        }
+    } else {
+        return ESP_ERR_NO_MEM;
+    }
+    return ESP_OK;
+}
+
+esp_err_t esp_rmaker_wifi_reset(int8_t reset_seconds, int8_t reboot_seconds)
+{
+    esp_err_t err = ESP_FAIL;
+    /* If reset time is 0, just do it right away */
+    if (reset_seconds == 0) {
+        err = __esp_rmaker_wifi_reset(reboot_seconds);
+    } else {
+        /* Else start a timer so that the task gets performed after the specified delay */
+        err = esp_rmaker_start_reset_timer(reset_seconds, reboot_seconds, esp_rmaker_wifi_reset_cb);
+    }
+    if (err == ESP_OK) {
+        esp_event_post(RMAKER_COMMON_EVENT, RMAKER_EVENT_WIFI_RESET, NULL, 0, portMAX_DELAY);
+    }
+    return ESP_OK;
+}
+
+esp_err_t esp_rmaker_factory_reset(int8_t reset_seconds, int8_t reboot_seconds)
+{
+    esp_err_t err = ESP_FAIL;
+    /* If reset time is 0, just do it right away */
+    if (reset_seconds == 0) {
+        err = __esp_rmaker_factory_reset(reboot_seconds);
+    } else {
+        /* Else start a timer so that the task gets performed after the specified delay */
+        err = esp_rmaker_start_reset_timer(reset_seconds, reboot_seconds, esp_rmaker_factory_reset_cb);
+    }
+    if (err == ESP_OK) {
+        esp_event_post(RMAKER_COMMON_EVENT, RMAKER_EVENT_FACTORY_RESET, NULL, 0, portMAX_DELAY);
+    }
+    return ESP_OK;
 }
