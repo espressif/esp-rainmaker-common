e0fbfe51c6b01588b08a1ab87d3f68de9c047ba8
Vikram Dattu <vikram.dattu@espressif.com>
2025-10-06 18:24:10 +0530

    Expose esp_rmaker_common_register_commands API
     - This can help registering the rmaker_common APIs while not using rmaker_common console init

--- src/console/rmaker_console.c
+++ src/console/rmaker_console.c
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2022-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -14,7 +14,6 @@
 #include <driver/uart.h>
 
 #include <esp_rmaker_common_console.h>
-#include "rmaker_console_internal.h"
 
 static const char *TAG = "esp_rmaker_console";
 static int stop;
